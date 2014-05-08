a181cb5eb716629db7be265c40a20a093d55ecaa
valenpunki@gmail.com <Victor@ValenPan.local>
2014-05-08 16:28:46 +0200

    timeline

---
+++ client/views/timeline/timeline.js
@@ -0,0 +1,29 @@
+Template.timeline.rendered = function() {
+
+ var s = document.getElementById("svgcontainer").scrollHeight;
+
+
+ 
+  $('#svgcontainer').mousemove(function(e){
+
+  	//segun distancia de scroll pone un filtro, podríamos hacer efecto parallax..
+ 
+
+    st = e.pageY - $(this).position().top;
+
+    
+
+  
+
+   
+
+    $('#svgcontainer:hover svg line').css({"-webkit-transform": "translateY("+Math.floor(st) + "px)" });
+
+    
+     /*$('#backgroundimage').css({"-webkit-filter": "grayscale("+st/500+""});*/
+
+    });
+
+
+
+}
